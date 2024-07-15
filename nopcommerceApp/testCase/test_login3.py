516bafbc4da753a3b686f0552c4f4d593ce701c5
Revnath Balu Shinde <rspatil5152@gmail.com>
2024-07-15 23:29:31 +0530

    develop unit test framework

---
+++ nopcommerceApp/testCase/test_login3.py
@@ -0,0 +1,4 @@
+from pageObject.LoginPage import LoginPage
+from selenium.webdriver.common.by import By
+from utilities.readProperties import ReadConfig
+from utilities.customLogger import setup_logging
