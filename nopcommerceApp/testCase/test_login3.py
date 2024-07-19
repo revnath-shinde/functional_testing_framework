71356b1495523756611be62938e55dc5edd6bbaf
Revnath Balu Shinde <rspatil5152@gmail.com>
2024-07-19 12:49:00 +0530

    letest commit

--- nopcommerceApp/testCase/test_login3.py
+++
@@ -1,4 +0,0 @@
-from pageObject.LoginPage import LoginPage
-from selenium.webdriver.common.by import By
-from utilities.readProperties import ReadConfig
-from utilities.customLogger import setup_logging
